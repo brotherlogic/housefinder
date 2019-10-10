b94eaf66c5206d6b29ff1cf1c15c255d5f394945
Simon Tucker <simon@brotherlogic@gmail>
2019-10-10 16:19:29 -0700

    Enables basic reading in update

--- housefinderutils_test.go
+++ housefinderutils_test.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"testing"
+
+	"golang.org/x/net/context"
 )
 
-func TestRun(t *testing.T) {
-	run()
+type testGetter struct {
+	httpGet func(ctx context.Context, url string) (string, error)
+}
+
+func (t *testGetter) get(ctx context.Context, url string) (string, error) {
+	return "got", nil
+}
+
+func InitTest() *Server {
+	s := Init()
+	s.getter = &testGetter{}
+	return s
+}
+
+func TestProcess(t *testing.T) {
+	s := InitTest()
+
+	err := s.processHouse(context.Background(), int32(123))
+
+	if err != nil {
+		t.Errorf("Error processing house")
+	}
 }
