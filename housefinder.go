e050036e32e556e082e943b8ac2d6b89be54c8d7
Simon Tucker <simon@brotherlogic@gmail>
2019-10-11 06:22:12 -0700

    Clear

--- housefinder.go
+++ housefinder.go
@@ -53,9 +53,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{
-		&pbg.State{Key: "what", Value: int64(123)},
-	}
+	return []*pbg.State{}
 }
 
 type getter interface {
